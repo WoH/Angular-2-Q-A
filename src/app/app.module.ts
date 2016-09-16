c5eb840b52b9b36482eb55e41983360db4b87e6e
Wolfgang Hobmaier <wolfgang.hobmaier@datapweb.de>
2016-09-16 20:32:06 +0200

    AngularFire Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { MdButtonModule } from '@angular2-material/button';
 import { MdCardModule } from '@angular2-material/card';
 import { MdInputModule } from '@angular2-material/input';
 import { MdToolbarModule } from '@angular2-material/toolbar';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { QuestionListComponent } from './components/question-list/question-list.component';
 import { QuestionPoseComponent } from './components/question-pose/question-pose.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbxVFNtwI437ePK5NfPAazfD_oTuDqvmE",
+  authDomain: "angular2-qa.firebaseapp.com",
+  databaseURL: "https://angular2-qa.firebaseio.com",
+  storageBucket: "angular2-qa.appspot.com",
+  messagingSenderId: "227115473860"
+};
 
 @NgModule({
   declarations: [
@@ -25,6 +34,7 @@ import { QuestionPoseComponent } from './components/question-pose/question-pose.
     MdCardModule,
     MdInputModule,
     MdToolbarModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
