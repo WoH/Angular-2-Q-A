9da05577f3404b05caabc1975382a0522c364be0
Wolfgang Hobmaier <wolfgang.hobmaier@datapweb.de>
2016-09-16 20:33:12 +0200

    QuestionList and QuestionPose Initial Draft

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Question } from '../../models/question';
 
 @Component({
   selector: 'app-question-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent implements OnInit {
+  questions: FirebaseListObservable<any>;
 
-  constructor() { }
+  constructor(af: AngularFire) { 
+    this.questions = af.database.list('/questions');
+  }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngOnDestroy() {
+    
   }
 
 }
--- src/app/components/question-pose/question-pose.component.ts
+++ src/app/components/question-pose/question-pose.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Question } from '../../models/question'
 
 @Component({
   selector: 'app-question-pose',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-pose.component.css']
 })
 export class QuestionPoseComponent implements OnInit {
+  question: Question;
 
-  constructor() { }
+  constructor(private af: AngularFire) { 
+    this.question = new Question();
+  }
 
   ngOnInit() { }
 
-  onAskClick() { }
+  onAskClick() {
+    const itemObservable = this.af.database.list('questions');
+    itemObservable.push(this.question);
+    this.question.title = "";
+    this.question.content = "";
+   }
 }
