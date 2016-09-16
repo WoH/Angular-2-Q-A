2b58d52cb1f555944ba8223271bb3b3d27816c9d
Wolfgang Hobmaier <wolfgang.hobmaier@datapweb.de>
2016-09-16 19:31:54 +0200

    TS config

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,5 +3,7 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
-declare var require: NodeRequire;
-declare var module: NodeModule;
+declare var require: any;
+declare var module: any;
+declare var HammerManager: any;
+
