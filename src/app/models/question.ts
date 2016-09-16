c83bdf6045386c464765766ec373ccf63bea3bb5
Wolfgang Hobmaier <wolfgang.hobmaier@datapweb.de>
2016-09-16 21:53:15 +0200

    QuestionModel: Add Category and upvote()

--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,13 +1,21 @@
+export type Category = 'Angular' | 'React' | 'Backbone' | 'General';
+
 export class Question {
     author: string;
     title: string;
     content: string;
     upvotes: number;
+    category: Category;
 
     constructor() {
         this.author = "";
         this.title = "";
         this.content = "";
         this.upvotes = 0;
+        this.category = 'General';
+    }
+
+    public upvote() {
+        this.upvotes++;
     }
 }
